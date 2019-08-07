828b9ed4047fcd0c8c1b1a577b4bf0a3fcb1f94f
liam chen <t.goo.dark@gmail.com>
2019-08-07 12:28:01 +0800

    remove unused api

--- stock_website/server/app/__init__.py
+++ stock_website/server/app/__init__.py
@@ -14,17 +14,13 @@ app = Flask(__name__,template_folder="templates",static_folder="static",static_u
 
 # enable CORS
 CORS(app)
-# app.config.from_object(__name__)
 
 app.config.from_object(config["default"])
 app.register_blueprint(main.main)
-app.register_blueprint(api.api)
 # app.config.from_object(config["ItemStorage"])
 # SQLAlchemy
 db = SQLAlchemy(app)
 # db2019 = SQLAlchemy(app)
-
-# 2018
 db2018 = SQLAlchemy(app)
 db2017 = SQLAlchemy(app)
 from app import models,views
